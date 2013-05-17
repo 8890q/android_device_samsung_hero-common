0b24c2860b0c58f282ea4161d901add9e8a89bf9
Steve Kondik <shade@chemlab.org>
2013-05-17 13:10:19 -0700

    hero-common: increase heap start size to 16m to minimize GC with larger bitmaps
    Change-Id: I89af5e94c9aafdc6007c84d3fce60e4db349e967

--- system_prop.mk
+++ system_prop.mk
@@ -21,7 +21,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.image-dex2oat-filter=speed
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
