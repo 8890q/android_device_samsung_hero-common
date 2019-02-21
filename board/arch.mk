690fbdbd4473e14d555e7cf0d228b9d097a3bfbf
Anton Hansson <hansson@google.com>
2019-02-21 15:04:20 +0000

    hero-common: Set cpu "runtime variant" to exynos-m1
    This makes bionic and various art tools that compile/runs cpu-specific
    code choose the kryo variant at runtime. It does not affect
    build-time compilation.
    
    Bug: 120773446
    Test: bionic benchmarks with and without this change
    Change-Id: I670c8acd094090135515688c5f057c2cc08a083f

--- board/arch.mk
+++ board/arch.mk
@@ -4,6 +4,7 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := exynos-m1
 
 # Secondary Architecture
 TARGET_2ND_ARCH := arm
@@ -11,3 +12,4 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53.a57
