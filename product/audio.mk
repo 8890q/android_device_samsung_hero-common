bcc644b716400587cd23c2a63ac7d48a68c142e3
ivanmeler <i_ivan@windowslive.com>
2020-01-22 14:16:05 +0000

    Build 32 bit audio hal only, Set up soundfx and back to stock audio_policy_configuration

--- product/audio.mk
+++ product/audio.mk
@@ -3,16 +3,18 @@ DEVICE_PATH := device/samsung/hero-common
 # Configs
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-	$(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+	$(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml \
+	$(DEVICE_PATH)/configs/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml
 
 # Packages
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
 	audio.usb.default \
 	audio.r_submix.default \
-	audio.primary.universal8890 \
+	audio.primary.universal8890_32 \
 	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl
+	android.hardware.audio.effect@2.0-impl \
+	libtinycompress
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
