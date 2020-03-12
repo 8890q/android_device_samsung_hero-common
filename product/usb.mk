3cb768b2233b115ca1d1998d50b2313e6e0b137a
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-03-12 21:05:16 +0100

    hero-common: build common basic usb service

--- product/usb.mk
+++ product/usb.mk
@@ -4,6 +4,4 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
-    android.hardware.usb@1.0-service.hero
-    
+    android.hardware.usb@1.0-service.basic
