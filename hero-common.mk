88c75175c605c65c4b44e9e25c6faf3a06460cd6
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-03-12 15:09:49 +0100

    hero-common: build SamsungDoze from hardware/samsung

--- hero-common.mk
+++ hero-common.mk
@@ -79,7 +79,7 @@ PRODUCT_PACKAGES += \
 	libsamsung_symbols \
 	SamsungServiceMode \
 	Torch \
-        HeroDoze \
+        SamsungDoze \
 	thermal.universal8890 \
 	android.hidl.base@1.0 \
 	android.hidl.manager@1.0
